7191f15082757efab35f98c5ac510213d42fb6a7
Nicolás Cardiel <cardiel@ucm.es>
2024-01-10 14:48:10 +0100

    Update version number

--- src/teareduce/__init__.py
+++ src/teareduce/__init__.py
@@ -12,10 +12,14 @@ from .cosmicrays import cr2images, apply_cr2images_ccddata, crmedian
 from .ctext import ctext
 from .draw_rectangle import draw_rectangle
 from .imshow import imshow
+from .peaks_spectrum import find_peaks_spectrum, refine_peaks_spectrum
+from .polfit import polfit_residuals, polfit_residuals_with_sigma_rejection
+from .robust_std import robust_std
+from .sdistortion import fit_sdistortion
 from .sliceregion import SliceRegion1D, SliceRegion2D
 from .statsummary import ifc_statsummary, statsummary
-from .robust_std import robust_std
 from .version import version
+from .wavecal import TeaWaveCalibration, apply_wavecal_ccddata
 from .zscale import zscale
 
 __version__ = version
--- src/teareduce/version.py
+++ src/teareduce/version.py
@@ -8,7 +8,7 @@
 # License-Filename: LICENSE.txt
 #
 
-version = '0.1.0'
+version = '0.2.0'
 
 
 def main():
