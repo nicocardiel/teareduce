1e80e445e6429f699b2eb60e2b016353ae1652d0
Nicolás Cardiel <cardiel@ucm.es>
2024-01-10 12:38:20 +0100

    Include functionality originally in tea_utils.py

--- src/teareduce/__init__.py
+++ src/teareduce/__init__.py
@@ -7,7 +7,15 @@
 # License-Filename: LICENSE.txt
 #
 
-
+from .avoid_astropy_warnings import avoid_astropy_warnings
+from .cosmicrays import cr2images, apply_cr2images_ccddata, crmedian
+from .ctext import ctext
+from .draw_rectangle import draw_rectangle
+from .imshow import imshow
+from .sliceregion import SliceRegion1D, SliceRegion2D
+from .statsummary import ifc_statsummary, statsummary
+from .robust_std import robust_std
 from .version import version
+from .zscale import zscale
 
 __version__ = version
