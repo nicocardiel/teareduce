513148974cae7fa537912650a2cb8081de196917
Nicolás Cardiel <cardiel@ucm.es>
2024-03-13 09:16:53 +0100

    Add dim/faint style in ctext() function

--- src/teareduce/ctext.py
+++ src/teareduce/ctext.py
@@ -13,6 +13,7 @@ def ctext(s=None,
           bg=None, 
           under=False,
           rev=False,
+          faint=False,
           bold=False):
     """Return coloured string using ANSI Escape Sequences
 
@@ -31,6 +32,8 @@ def ctext(s=None,
         If True, add underline style.
     rev : bool
         If True, add reverse style.
+    faint : bool
+        If True, add dim/faint style.
     bold : bool
         If True, add bold style
 
@@ -76,6 +79,8 @@ def ctext(s=None,
         style_list.append('\x1B[4m')
     if rev:
         style_list.append('\x1B[7m')
+    if faint:
+        style_list.append('\x1B[2m')
     if bold:
         style_list.append('\x1B[1m')
 
