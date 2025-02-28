0ad1556534a2d8ec59046ba692e35c6efee482c8
Nicolás Cardiel <cardiel@ucm.es>
2025-02-28 12:10:21 +0100

    Update slice classes.
    Allow to use a simple string "[num1:num2]" or "[num1:num2,num3:num4]" to define the slice regions.
    
    Include docstring documentation.

--- src/teareduce/sliceregion.py
+++ src/teareduce/sliceregion.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2022-2024 Universidad Complutense de Madrid
+# Copyright 2022-2025 Universidad Complutense de Madrid
 #
 # This file is part of teareduce
 #
@@ -7,18 +7,62 @@
 # License-Filename: LICENSE.txt
 #
 
+import numpy as np
+import re
 
 class SliceRegion1D:
     """Store indices for slicing of 1D regions.
     
     The attributes .python and .fits provide the indices following
     the Python and the FITS convention, respectively.
-    
+
+    Attributes
+    ----------
+    fits : slice
+        1D slice following the FITS convention.
+    python : slice
+        1D slice following the Python convention.
+    mode : str
+        Convention mode employed to define the slice.
+        The two possible modes are 'fits' and 'python'.
+    fits_section : str
+        Resulting slice section in FITS convention: '[num1:num2]'.
+        This string is defined after successfully initializing
+        the SliceRegion1D instance.
+
+    Methods
+    -------
+    within(other)
+        Check if slice 'other' is within the parent slice.
     """
     
     def __init__(self, region, mode=None):
+        """Initialize SliceRegion1D.
+
+        Parameters
+        ----------
+        region : slice or str
+            Slice region. It can be provided as np.s_[num1:num2],
+            as slice(num1, num2) or as a string '[num1:num2]'
+        mode : str
+            Convention mode employed to define the slice.
+            The two possible modes are 'fits' and 'python'.
+        """
+        if isinstance(region, str):
+            pattern = r'^\s*\[\s*\d+\s*:\s*\d+\s*\]\s*$'
+            if not re.match(pattern, region):
+                raise ValueError(f"Invalid {region!r}. It must match '[num:num]'")
+            # extract numbers and generate np.s_[num:num]
+            numbers_str = re.findall(r'\d+', region)
+            print(f'{numbers_str=}')
+            numbers_int = list(map(int, numbers_str))
+            print(f'{numbers_int=}')
+            region = np.s_[numbers_int[0]:numbers_int[1]]
+
         if isinstance(region, slice):
-            pass
+            for number in [slice.start, slice.stop]:
+                if number is None:
+                    raise ValueError(f'Invalid {slice!r}: you must specify start:stop in slice by number')
         else:
             raise ValueError(f'Object {region} of type {type(region)} is not a slice') 
                              
@@ -44,6 +88,9 @@ class SliceRegion1D:
         else:
             raise ValueError(errmsg)
 
+        s = self.fits
+        self.fits_section = f'[{s.start}:{s.stop}]'
+
     def __eq__(self, other):
         return self.fits == other.fits and self.python == other.python
 
@@ -56,17 +103,34 @@ class SliceRegion1D:
                     f'{self.python!r}, mode="python")')
 
     def within(self, other):
+        """Determine if slice 'other' is within the parent slice.
+
+        Parameters
+        ----------
+        other : slice
+            SliceRegion1D instance for which we want to determine
+            if it is within the parent SliceRegion1D instance.
+
+        Returns
+        -------
+        result : bool
+            Return True if 'other' is within the parent slice.
+            False otherwise.
+        """
         if isinstance(other, self.__class__):
             pass
         else:
             raise ValueError(f'Object {other} of type {type(other)} is not a {self.__class__.__name__}')
-        region = self.python
-        region_other = other.python
-        if region.start < region_other.start:
-            return False
-        if region.stop > region_other.stop:
-            return False
-        return True
+
+        s = self.python
+        s_other = other.python
+        result = False
+        if s.start < s_other.start:
+            return result
+        if s.stop > s_other.stop:
+            return result
+        result = True
+        return result
                      
 
 class SliceRegion2D:
@@ -75,23 +139,62 @@ class SliceRegion2D:
     The attributes .python and .fits provide the indices following
     the Python and the FITS convention, respectively.
 
-    """
+    Attributes
+    ----------
+    fits : slice
+        1D slice following the FITS convention.
+    python : slice
+        1D slice following the Python convention.
+    mode : str
+        Convention mode employed to define the slice.
+        The two possible modes are 'fits' and 'python'.
+    fits_section : str
+        Resulting slice section in FITS convention:
+        '[num1:num2,num3:num4]'. This string is defined after
+        successfully initializing the SliceRegion2D instance.
+
+    Methods
+    -------
+    within(other)
+        Check if slice 'other' is within the parent slice."""
 
     def __init__(self, region, mode=None):
-        if len(region) != 2:
-            raise ValueError(f'This class {self.__class__.__name__} '
-                             'only handles 2D regions')
+        """Initialize SliceRegion1D.
 
-        s1, s2 = region
-        for item in [s1, s2]:
-            if isinstance(item, slice):
-                pass
-            else:
-                raise ValueError(f'Object {item} of type {type(item)} is not a slice')
+        Parameters
+        ----------
+        region : slice or str
+            Slice region. It can be provided as np.s_[num1:num2, num3:num4],
+            as a tuple (slice(num1, num2), slice(num3, num4)),
+            or as a string '[num1:num2, num3:num4]'
+        mode : str
+            Convention mode employed to define the slice.
+            The two possible modes are 'fits' and 'python'.
+        """
+        if isinstance(region, str):
+            pattern = r'^\s*\[\s*\d+\s*:\s*\d+\s*,\s*\d+\s*:\s*\d+\s*\]\s*$'
+            if not re.match(pattern, region):
+                raise ValueError(f"Invalid {region!r}. It must match '[num:num, num:num]'")
+            # extract numbers and generate np.s_[num:num, num:num]
+            numbers_str = re.findall(r'\d+', region)
+            print(f'{numbers_str=}')
+            numbers_int = list(map(int, numbers_str))
+            print(f'{numbers_int=}')
+            region = np.s_[numbers_int[0]:numbers_int[1], numbers_int[2]:numbers_int[3]]
 
-        if s1.step not in [1, None] or s2.step not in [1, None]:
-            raise ValueError(f'This class {self.__class__.__name__} '
-                             'does not handle step != 1')
+        if isinstance(region, tuple) and len(region) == 2:
+            s1, s2 = region
+            for item in [s1, s2]:
+                if isinstance(item, slice):
+                    for number in [s1.start, s1.stop, s2.start, s2.stop]:
+                        if number is None:
+                            raise ValueError(f'Invalid {item!r}: you must specify start:stop in slice by number')
+                else:
+                    raise ValueError(f'Object {item} of type {type(item)} is not a slice')
+            if s1.step not in [1, None] or s2.step not in [1, None]:
+                raise ValueError(f'This class {self.__class__.__name__} does not handle step != 1')
+        else:
+            raise ValueError(f'This class {self.__class__.__name__} only handles 2D regions')
 
         errmsg = f'Invalid mode={mode}. Only "FITS" or "Python" (case insensitive) are valid'
         if mode is None:
@@ -116,7 +219,7 @@ class SliceRegion2D:
             raise ValueError(errmsg)
 
         s1, s2 = self.fits
-        self.fits_section = f'[{s1.start}:{s1.stop}, {s2.start}:{s2.stop}]'
+        self.fits_section = f'[{s1.start}:{s1.stop},{s2.start}:{s2.stop}]'
 
     def __eq__(self, other):
         return self.fits == other.fits and self.python == other.python
@@ -130,6 +233,20 @@ class SliceRegion2D:
                     f'{self.python!r}, mode="python")')
 
     def within(self, other):
+        """Determine if slice 'other' is within the parent slice.
+
+        Parameters
+        ----------
+        other : slice
+            SliceRegion2D instance for which we want to determine
+            if it is within the parent SliceRegion2D instance.
+
+        Returns
+        -------
+        result : bool
+            Return True if 'other' is within the parent slice.
+            False otherwise.
+        """
         if isinstance(other, self.__class__):
             pass
         else:
@@ -137,12 +254,14 @@ class SliceRegion2D:
 
         s1, s2 = self.python
         s1_other, s2_other = other.python
+        result = False
         if s1.start < s1_other.start:
-            return False
+            return result
         if s1.stop > s1_other.stop:
-            return False
+            return result
         if s2.start < s2_other.start:
-            return False
+            return result
         if s2.stop > s2_other.stop:
-            return False
-        return True
+            return result
+        result = True
+        return result
