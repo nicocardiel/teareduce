3f5a9b886a74d4afea8e020c0d929f5b80e729c5
Nicolás Cardiel <cardiel@ucm.es>
2025-12-01 11:39:55 +0100

    Facilitate cleanest import

---
+++ src/teareduce/cleanest/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright 2023-2025 Universidad Complutense de Madrid
+#
+# This file is part of teareduce
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+from .cleanest import cleanest
